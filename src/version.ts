fa1305e9e4a961f0fa82fa7e4f3832f1cadea5d6
LuisAngelGGG <devjsluis@gmail.com>
2026-05-07 10:16:39 -0600

    update version

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // App version – auto-bumped on every build by vite.config.ts
-export const APP_VERSION = '1.0.43';
-export const APP_BUILD_DATE = '2026-05-04 16:04';
+export const APP_VERSION = '1.0.44';
+export const APP_BUILD_DATE = '2026-05-07 16:12';
